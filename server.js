a6fd6ba0241aa4b25bc90c27a4c50ca962c94f34
FLOWOLFOX <flowolfox@gmail.com>
2024-04-01 18:41:10 +0900

    Challenge-7

--- server.js
+++ server.js
@@ -18,3 +18,9 @@ app.get('/', (req, res) => {
 app.listen(PORT, () => {
   console.log('listening on port ' + PORT);
 });
+
+app.use((error, req, res, next) => {
+  res.status(500).json({ message: error.message });
+});
+
+module.exports = app;
