ce3e1546e08c24d59eb32a9198149d1e92a2b9ed
FLOWOLFOX <flowolfox@gmail.com>
2024-04-01 10:54:27 +0900

    Challenge-6

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+const PORT = 4000;
+const productRoutes = require('./routes');
+const mongoose = require('mongoose');
+app.use(express.json());
+
+mongoose.connect('mongodb+srv://flowolfox:test123@cluster0.lck5f51.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
+  .then(() => console.log('MongoDB Connected.'))
+  .catch((error) => console.error(error));
+
+app.use("/api/products", productRoutes);
+
+app.get('/', (req, res) => {
+  res.send('Hello World');
+});
+
+app.listen(PORT, () => {
+  console.log('listening on port ' + PORT);
+});
